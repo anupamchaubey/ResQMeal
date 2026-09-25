692fe8aaff566927e927e93d2e01bf08fcb0519b
Anupam Chaubey <icyanupam@gmail.com>
2026-09-26 02:30:34 +0530

    setup ResQMeal - Food Rescue Engine

---
+++ src/main/java/projects/ResQMeal/ResQMealApplication.java
@@ -0,0 +1,13 @@
+package projects.ResQMeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResQMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResQMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projects/ResQMeal/ResQMealApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.ResQMeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResQMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
